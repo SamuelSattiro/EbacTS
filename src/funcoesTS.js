ebedce6653b149c2a543e69ca975fd7d8995d1f4
Samuel Sattiro <Samucasupertramp@gmail.com>
2024-11-22 15:09:59 -0300

    first commit

---
+++ src/funcoesTS.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicaNumeros(num1, num2) {
+    return num1 * num2;
+}
+function dizOi(nome) {
+    return `Oi ${nome}! Espero que você esteja bem!`;
+}
+console.log(multiplicaNumeros(25, 4));
+console.log(dizOi('Samuel'));
---
+++ src/funcoesTS.ts
@@ -0,0 +1,10 @@
+function multiplicaNumeros (num1: number, num2: number): number {
+    return num1 * num2
+}
+
+function dizOi(nome: string): string {
+    return `Oi ${nome}! Espero que você esteja bem!`
+}
+
+console.log(multiplicaNumeros(25, 4))
+console.log(dizOi('Samuel'))
